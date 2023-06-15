905b7c590731c76eeb62eb0c1c5e75a26caed112
cesarcarrera93 <cesarcarrera93@hotmail.com>
2023-06-15 12:37:39 -0300

    commit refactor autenticación

---
+++ CajeroAutomatico/CajeroAutomatico/Scripts/Acceso/index.js
@@ -0,0 +1,75 @@
+﻿$(document).ready(function () {
+    $('.keypad-button').click(function () {
+        var number = $(this).data('number');
+        appendNumber(number);
+    });
+
+    $('.clear-button').click(function () {
+        clearInput();
+    });
+
+    $('.accept-button').click(function () {
+        submitInput();
+    });
+
+    updateAcceptButton();
+});
+
+function appendNumber(number) {
+    var inputField = $('#inputField');
+    var currentValue = inputField.val();
+    var newValue = currentValue + number;
+    if (newValue.length <= 19) {
+        newValue = formatCardNumber(newValue);
+        inputField.val(newValue);
+    }
+
+    updateAcceptButton();
+}
+
+function clearInput() {
+    var inputField = $('#inputField');
+    inputField.val('');
+
+    updateAcceptButton();
+}
+
+function submitInput() {
+    var cardNumber = $('#inputField').val().replace(/-/g, '');
+
+    $.ajax({
+        url: '/Acceso/ValidarTarjeta',
+        method: 'GET',
+        data: { cardNumber: cardNumber },
+        success: function (response) {
+            console.log(response);
+            //if valido 
+            window.location.href = '/Acceso/Pin'
+
+            // else
+            // alert('La tarjeta esta bloqueada o no existe');
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
+
+function formatCardNumber(cardNumber) {
+    var formattedNumber = cardNumber.replace(/\D/g, ''); // Eliminar caracteres no numéricos
+    var regex = /(\d{4})(?=\d)/g; // Expresión regular para separar en grupos de 4 dígitos
+    formattedNumber = formattedNumber.replace(regex, '$1-');
+    return formattedNumber;
+}
+
+function updateAcceptButton() {
+    var inputField = $('#inputField');
+    var cardNumber = inputField.val().replace(/-/g, '');
+    var acceptButton = $('.accept-button');
+
+    if (cardNumber.length === 16) {
+        acceptButton.removeClass('disabled');
+    } else {
+        acceptButton.addClass('disabled');
+    }
+}
---
+++ CajeroAutomatico/CajeroAutomatico/Scripts/Acceso/pin.js
@@ -0,0 +1,78 @@
+﻿$(document).ready(function () {
+    $('.keypad-button').click(function () {
+        var number = $(this).data('number');
+        appendNumber(number);
+    });
+
+    $('.clear-button').click(function () {
+        clearInput();
+    });
+
+    $('.accept-button').click(function () {
+        submitInput();
+    });
+
+    $('#exitButton').click(function () {
+        goBack();
+    });
+
+    updateAcceptButton();
+});
+
+function appendNumber(number) {
+    var inputField = $('#inputField');
+    var currentValue = inputField.val();
+    var newValue = currentValue + number;
+    if (newValue.length <= 4) {
+        inputField.val(newValue);
+    }
+
+    updateAcceptButton();
+}
+
+function clearInput() {
+    var inputField = $('#inputField');
+    inputField.val('');
+
+    updateAcceptButton();
+}
+
+function submitInput() {
+    var cardNumber = $('#inputField').val().replace(/-/g, '');
+
+    $.ajax({
+        url: '/Acceso/ValidarPin',
+        method: 'GET',
+        data: { cardNumber: cardNumber },
+        success: function (response) {
+            console.log(response);
+            // if contraseña valida
+            window.location.href = '/Operacion/Index'
+
+            // else if reintento < 4
+            // cartel aviso reintento
+
+            // else 
+            //window.location.href = '/Acceso/Bloqueo'
+        },
+        error: function (error) {
+            console.error(error);
+        }
+    });
+}
+
+function goBack() {
+    window.history.back();
+}
+
+function updateAcceptButton() {
+    var inputField = $('#inputField');
+    var cardNumber = inputField.val().replace(/-/g, '');
+    var acceptButton = $('.accept-button');
+
+    if (cardNumber.length === 4) {
+        acceptButton.removeClass('disabled');
+    } else {
+        acceptButton.addClass('disabled');
+    }
+}
