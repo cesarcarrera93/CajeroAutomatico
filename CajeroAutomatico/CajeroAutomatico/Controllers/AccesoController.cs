615da1cb041acb3eed1c9b4dab725fe2e70f1443
cesarcarrera93 <cesarcarrera93@hotmail.com>
2023-06-15 03:56:01 -0300

    commit falta pin
    listo:
    -pantalla tarjeta
    
    falta terminar:
    - pantalla pin (boton aceptar)

---
+++ CajeroAutomatico/CajeroAutomatico/Controllers/AccesoController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CajeroAutomatico.Controllers
+{
+    public class AccesoController : Controller
+    {
+        // GET: Acceso
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Pin()
+        {
+            return View();
+        }
+
+        public ActionResult ValidarTarjeta(string cardNumber)
+        {
+            try
+            {
+                return Content("1");
+            }
+            catch (Exception ex) 
+            {
+                return Content("ocurrio un error: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ CajeroAutomatico/CajeroAutomatico/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CajeroAutomatico.MvcApplication" Language="C#" %>
